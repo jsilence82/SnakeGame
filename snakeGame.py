c1d34c5ff23be021e5ac5614065cc13585a7313f
jsilence82 <jeff.silence@gmail.com>
2022-05-16 13:48:55 +0200

    Initial commit

---
+++ snakeGame.py
@@ -0,0 +1,18 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+dis_width = 600
+dis_height = 400
+
+
+clock = pygame.time.Clock()
